28eb2327dab3591d63652effc7bd9bce1c8f7d2f
Marcin <marcin.jakubkrysinski@gmail.com>
2020-10-18 22:38:59 +0200

    add new gui components

--- src/main/java/pl/krysinski/db/gui/CarDbGui.java
+++ src/main/java/pl/krysinski/db/gui/CarDbGui.java
@@ -1,6 +1,6 @@
 package pl.krysinski.db.gui;
 
-import com.vaadin.flow.component.Component;
+
 import com.vaadin.flow.component.button.Button;
 import com.vaadin.flow.component.grid.Grid;
 import com.vaadin.flow.component.orderedlayout.HorizontalLayout;
@@ -10,10 +10,8 @@ import com.vaadin.flow.component.textfield.TextField;
 import com.vaadin.flow.router.Route;
 import org.springframework.beans.factory.annotation.Autowired;
 import pl.krysinski.db.model.Car;
-import pl.krysinski.db.service.CarService;
 import pl.krysinski.db.service.CarServiceImpl;
 
-import java.awt.*;
 
 @Route("")
 public class CarDbGui extends HorizontalLayout {
@@ -23,30 +21,59 @@ public class CarDbGui extends HorizontalLayout {
     Grid<Car> grid = new Grid<>(Car.class);
 
 
+
     @Autowired
     public CarDbGui(CarServiceImpl carService) {
         this.carService = carService;
 
         addCarFields();
         getCars();
+
     }
 
     public void getCars(){
         grid.setItems(carService.getCarList());
         grid.setColumns("carsId", "mark", "model", "color", "dateProduction");
+        grid.setHeight("530px");
         add(grid);
     }
 
     public void addCarFields(){
         VerticalLayout verticalLayout = new VerticalLayout();
-        NumberField carId = new NumberField("ID");
-        TextField carMark = new TextField("Mark");
-        TextField carModel = new TextField("Model");
-        TextField carColor = new TextField("Color");
-        NumberField carDateProduction = new NumberField("Date production");
+        NumberField carId = new NumberField("Add car:");
+        TextField carMark = new TextField();
+        TextField carModel = new TextField();
+        TextField carColor = new TextField();
+        NumberField carDateProduction = new NumberField();
         Button addButton = new Button("Add");
+
+        TextField fromField = new TextField("Search by year of production");
+        TextField toField = new TextField();
+        Button searchButton = new Button("Search");
+
+        NumberField idField = new NumberField("Delete car by id:");
+        Button deleteButton = new Button("Delete");
+
+        carId.setPlaceholder("id");
+        carMark.setPlaceholder("mark");
+        carModel.setPlaceholder("model");
+        carColor.setPlaceholder("color");
+        carDateProduction.setPlaceholder("year of production:");
+        carDateProduction.setWidth("190px");
+        addButton.addThemeName("primary");
+
+        fromField.setPlaceholder("From:");
+        toField.setPlaceholder("To:");
+        searchButton.addThemeName("primary");
         verticalLayout.setWidth("210px");
-        verticalLayout.add(carId, carMark, carModel, carColor, carDateProduction, addButton);
+        verticalLayout.add(fromField, toField, searchButton);
+        add(verticalLayout);
+
+        idField.setPlaceholder("Id");
+        deleteButton.addThemeName("primary");
+
+        verticalLayout.setWidth("210px");
+        verticalLayout.add(carId, carMark, carModel, carColor, carDateProduction, addButton, idField,deleteButton);
         add(verticalLayout);
 
     }
